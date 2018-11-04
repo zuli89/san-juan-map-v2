178b4039a90496f488b67e02ab1e3183550212ca
Zulimar Lucena <zulimar.lucena@gmail.com>
2018-11-03 19:45:26 -0500

    Added markers, infowindow

--- src/App.js
+++ src/App.js
@@ -2,10 +2,64 @@ import React, { Component } from 'react';
 import Sidebar from './components/Sidebar'
 import fsAPI from './fsAPI'
 import Map from "./components/Map"
-
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: []
+    };
+  }
+
+  markerClick = marker => { //manages clicks on markers
+    this.closeOpenWindow();
+    marker.isOpen = true; //when clicked, sets marker to isOpen
+    this.setState({markers: Object.assign(this.state.markers, marker) }); 
+    const venue = this.state.venues.find(venue => venue.id === marker.id); //matches the selected marker with the venue information
+    fsAPI.getVenue(marker.id) //call for venue information
+      .then(res => {
+        const newVenue = Object.assign(venue, res.response.venue);
+        this.setState({ venues: Object.assign(this.state.venues, newVenue)});
+        console.log(newVenue)
+      })
+  };
+
+  closeOpenWindow = () => {
+    const markers = this.state.markers.map(marker => {
+      marker.isOpen = false;
+      return marker;
+    });
+    this.setState({markers: Object.assign(this.state.markers, markers) });
+  }
+
+
+
+  componentDidMount = () => {
+    fsAPI.search({ //calls search method for foursqaure API
+      ll:'18.466080,-66.115531',
+      radius : '450',
+      categoryId : '4bf58dd8d48988d144941735,4bf58dd8d48988d1be941735,4bf58dd8d48988d116941735,4bf58dd8d48988d11e941735,4bf58dd8d48988d16d941735',
+      limit: 40
+    })
+    .then(results => {
+      const {venues} = results.response;
+      const markers = venues.map(venue=> {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          id: venue.id,
+          isOpen: false,
+          isVisible: true
+        }
+      });
+    this.setState({venues, markers});
+    });
+  }
+  
+
   render() {
     return (
       <div>    
@@ -14,7 +68,7 @@ class App extends Component {
       </div>
       
       <Sidebar/>
-      <Map/>
+      <Map {...this.state} markerClick={this.markerClick}/>
 
     </div>
     );
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from "react-google-maps"
 
 const MyMapComponent = withScriptjs(withGoogleMap((props) =>
   <GoogleMap
     defaultZoom={16}
     defaultCenter={{ lat: 18.466080, lng: -66.115531 }}
-    options={{ scrollwheel: true}}
+    options={{ 
+      scrollwheel: true,
+    }}
   >
-    {props.isMarkerShown && <Marker position={{ lat: 18.466080, lng: -66.115531 }} />}
+  
+    {props.markers && props.markers
+      .filter(marker => marker.isVisible)
+      .map((marker, idx) => {
+          const venueInfo = props.venues.find(venue => venue.id === marker.id);
+        return <Marker key={idx} position={{ lat: marker.lat, lng: marker.lng }} 
+        onClick={() => props.markerClick(marker)}>
+          {marker.isOpen && venueInfo.bestPhoto && (<InfoWindow>
+            <div id='info'>
+              <p id = "venue-name"> {venueInfo.name} </p>
+              <p id = "venue-rating">{`Rating: ${venueInfo.rating}/10`}</p>
+              <p id = "venue-price">{`Price: ${venueInfo.price.message}`}</p>
+              <img id= "venue-img" src = {`${venueInfo.bestPhoto.prefix}200x200${venueInfo.bestPhoto.suffix}`} alt ={`${venueInfo.name}`}/>
+              <p id = "venue-address">{venueInfo.location.address}</p>
+            </div>
+          </InfoWindow>
+          )}
+        </Marker>
+    })}
   </GoogleMap>
 ))
 
@@ -18,13 +38,13 @@ export default class Map extends Component {
   render() {
     return (
       
-      <div id='map-loader'><MyMapComponent
-        isMarkerShown
-        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBLnDm2Kz95aYZj7970Lgo1shaeXodi-y4"
-        loadingElement={<div style={{ height: `100%` }} />}
-        containerElement={<div style={{ height: `100%`, width: `100%`}} />}
-        mapElement={<div style={{ height: `100%` }} />
-      }
+      <div id='map-loader'>
+        <MyMapComponent
+          {...this.props}
+          googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&key=AIzaSyBLnDm2Kz95aYZj7970Lgo1shaeXodi-y4"
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `100%`, width: `80%`, marginLeft: `20%`}} />}
+          mapElement={<div style={{ height: `100%` }} />}
         />
       </div>
     )
